d863b1cacb51549953f31f7511a14ee90b58dc7a
Milky.W <minniewu@qq.com>
2018-08-17 16:35:19 +0800

    Init

---
+++ Test/Assets/Scripts/Box.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Renderer>().enabled = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponent<Renderer>().enabled = true;
+    }
+}
---
+++ Test/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private const float one = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.Log("pengzhuangduixiang" + hit.collider.name);
+            GameObject go = GameObject.Find(hit.collider.name);
+            if (go.tag == "ghost")
+            {
+                go.SetActive(false);
+            }
+            //go.GetComponent<Renderer>().enabled = false;
+
+        }
+        //GetComponent<Transform>().Rotate(999, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Transform>().Rotate(-one, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Transform>().Rotate(one, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Transform>().Rotate(0, -one, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Transform>().Rotate(0, one, 0);
+        }
+    }
+
+}
