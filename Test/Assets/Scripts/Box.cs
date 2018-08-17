b61991d0c5ad3b9304f6e2058be0392fa009e9a5
zhanghaoqi39 <jjyy8763@163.com>
2018-08-17 17:09:25 +0800

    moving zombie beta

--- Test/Assets/Scripts/Box.cs
+++ Test/Assets/Scripts/Box.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Box : MonoBehaviour {
-
+    public float speed = 0.5f;
     // Use this for initialization
     void Start () {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void LateUpdate () {
+        
+        Vector3 target;
+        target = GameObject.Find("Flashlight").GetComponent<Transform>().position;
+        GetComponent<Transform>().position = GetComponent<Transform>().position + (target - GetComponent<Transform>().position) * speed* Time.deltaTime;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
